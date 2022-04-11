4b79b0a8c29e006dd07df83bcc4db3dc2a45a164
ohoadit <aditshah8@gmail.com>
2022-04-11 14:35:25 -0500

    port change, auth changes

--- index.js
+++ index.js
@@ -25,6 +25,6 @@ app.use("/tutorapi", tutorRouter);
 app.use("/studentapi", studentRouter);
 app.use("/appointmentapi", appointmentRouter);
 
-app.listen(3000, () => {
-  console.log(`Server Started at ${3000}`);
+app.listen(8000, () => {
+  console.log(`Server Started at ${8000}`);
 });
--- utils/token.js
+++ utils/token.js
@@ -1,8 +1,17 @@
 const jwt = require("jsonwebtoken")
 
-const verfiyToken = (token) => { // resolve logout(client side)
+// const verfiyToken = (token) => { // resolve logout(client side)
+//     try {
+//         return jwt.verify(token, process.env.JWT_KEY);
+//     } catch (err) {
+//         throw err
+//     }
+// }
+
+const verfiyTokenAndExtractInfo = (token, key = "_id") => {
     try {
-        return jwt.verify(token, process.env.JWT_KEY);
+        const data = jwt.verify(token, process.env.JWT_KEY);
+        return data[key];
     } catch (err) {
         throw err
     }
@@ -11,4 +20,4 @@ const verfiyToken = (token) => { // resolve logout(client side)
 const generateToken = (payload) => jwt.sign(payload, process.env.JWT_KEY);
 
 
-module.exports = { generateToken, verfiyToken }
+module.exports = { generateToken, verfiyTokenAndExtractInfo }
